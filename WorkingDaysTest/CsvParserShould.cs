4694b9b29dd14d665159fac51c35532694ced7e8
danieltran-97 <danielmctran@gmail.com>
2021-07-21 15:53:57 +1000

    sln

---
+++ WorkingDaysTest/CsvParserShould.cs
@@ -0,0 +1,32 @@
+using WorkingDays;
+using Xunit;
+
+namespace WorkingDaysTest
+{
+    public class CsvParserShould
+    {
+        [Fact]
+        public void ParseHolidaysCsv_should_return_a_list_of_91_holidays()
+        {
+            var csvParser = new CsvParser();
+        
+            var listOfHolidays = CsvParser.ParseHolidaysCsv();
+            
+            Assert.Equal(91, listOfHolidays.Count);
+        }
+
+        [Fact]
+        public void ParseHolidaysCsv_returns_information_with_commas()
+        {
+            var csvParser = new CsvParser();
+            
+            var holidayList = CsvParser.ParseHolidaysCsv();
+            var actual = holidayList[22].Information;
+            var expected =
+                "Always on a Monday, creating a long weekend. It celebrates the eight-hour working day, a victory for workers in the mid-late 19th century.";
+        
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
